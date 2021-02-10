c7f9b714c636963708316f0c1cfd76be902b5460
Serhii Yarovyi <cant1987m@gmail.com>
2021-02-10 10:49:34 +0200

    first commit

---
+++ config/routes.rb
@@ -0,0 +1,9 @@
+Rails.application.routes.draw do
+  # ActiveAdmin.routes(self)
+  # # devise_for :users
+  # resources :categories do
+  #   resources :products
+  # end
+  # root "pages#index"
+end
+
---
+++ config/webpack/environment.js
@@ -0,0 +1,12 @@
+const { environment } = require('@rails/webpacker')
+const jquery = require('./plugins/jquery')
+
+const webpack = require('webpack')
+environment.plugins.append('Provide', new webpack.ProvidePlugin({
+  $: 'jquery',
+  jQuery: 'jquery',
+  Popper: ['popper.js', 'default']
+}))
+
+environment.plugins.prepend('jquery', jquery)
+module.exports = environment
