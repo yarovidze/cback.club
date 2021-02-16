f6bb4dfc4390ccc5c0f9bfa8b9eaf568c84878c6
OleStan <59766354+OleStan@users.noreply.github.com>
2021-02-16 18:22:54 +0200

    finaly db

--- app/models/admin_user.rb
+++ app/models/admin_user.rb
@@ -1,9 +1,6 @@
-class User < ApplicationRecord
+class AdminUser < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
-  has_many :cards
-  has_many :favorites
-  has_many :transactions
   devise :database_authenticatable, 
          :recoverable, :rememberable, :validatable
 end
