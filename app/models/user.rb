a50e25b38ce62f366d5a5d9256c8d3bd544d3e6c
Yevhenii Kononenko <evgenijkononenko0@gmail.com>
2021-02-18 14:01:57 +0200

    facebook Oauth complete(localhost)

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,19 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  
   has_many :cards
   has_many :favorites
   has_many :transactions
   devise :database_authenticatable, 
-         :recoverable, :rememberable, :validatable
+         :recoverable, :rememberable, :validatable,:omniauthable, omniauth_providers: %i[facebook]
+
+  def self.from_omniauth(auth)
+    where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
+      user.email = auth.info.email
+      user.password = Devise.friendly_token[0,20]
+    end      
+  end
+    
+  
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
-  devise_for :users, ActiveAdmin::Devise.config
+  ActiveAdmin::Devise.config
+  
   ActiveAdmin.routes(self)
+  devise_for :users, :controllers => { :omniauth_callbacks => "users/omniauth_callbacks" }
   root "pages#index"
 end
 
