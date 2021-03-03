dff5daa266132e385316a6e6fb232c5248bbd815
Yevhenii Kononenko <evgenijkononenko0@gmail.com>
2021-03-03 18:00:13 +0200

    google recaptcha added

--- app/controllers/offers_controller.rb
+++ app/controllers/offers_controller.rb
@@ -4,6 +4,11 @@ class OffersController < ApplicationController
     @offers = Offer.paginate(page: params[:page], per_page: 16)
   end
 
+  def search
+    @offers = Offer.where('name ILIKE ?', "%#{params[:q]}%").paginate(page: params[:page], per_page: 16)
+    render :index
+  end
+
   def show
   end
 end
