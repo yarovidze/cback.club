44e60a30aa041ff610e17a9fd5129d9d9f27b0e8
Yevhenii Kononenko <evgenijkononenko0@gmail.com>
2021-03-05 09:25:49 +0200

    simple search added

--- app/controllers/offers_controller.rb
+++ app/controllers/offers_controller.rb
@@ -1,13 +1,8 @@
 class OffersController < ApplicationController
   def index
-    @offers = Offer.all
-    @offers = Offer.paginate(page: params[:page], per_page: 16)
+    @offers = Offer.where('name ILIKE ?', "%#{params[:q]}%").paginate(page: params[:page], per_page: 16)
   end
 
-  def search
-    @offers = Offer.where('name ILIKE ?', "%#{params[:q]}%").paginate(page: params[:page], per_page: 16)
-    render :index
-  end
 
  
   def show
