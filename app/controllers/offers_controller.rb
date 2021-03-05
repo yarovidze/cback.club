4fc401642b51cdc042336d9b49db9e9b9043066f
Yevhenii Kononenko <evgenijkononenko0@gmail.com>
2021-03-05 12:21:13 +0200

    autocomplete search completed

--- app/controllers/offers_controller.rb
+++ app/controllers/offers_controller.rb
@@ -1,10 +1,23 @@
 class OffersController < ApplicationController
   def index
-    @offers = Offer.where('name ILIKE ?', "%#{params[:q]}%").paginate(page: params[:page], per_page: 16)
+    @offers = Offer.where('name ILIKE ?', "%#{params[:query]}%").paginate(page: params[:page], per_page: 16)
+    respond_to do |format|
+      format.html
+      format.json { render json: @offers.map(&:name) }
+    end
+   
+  end
+
+  def search
+    @offers = Offer.where('name ILIKE ?', "%#{params[:query]}%").paginate(page: params[:page], per_page: 16)
+    render "offers/index"
+  end
+
+  def autocomplete
+    render json: Offer.all.map(&:name)
   end
 
 
- 
   def show
     @offer = Offer.find(params[:id])
   end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
 
   get 'offers/index'
   get 'offers/show'
+  get 'autocomplete', to: 'offers#autocomplete', as: 'autocomplete'
   devise_for :users, controllers: { omniauth_callbacks: 'users/omniauth_callbacks',registrations: 'users/registrations', sessions: 'users/sessions' }
   devise_scope :user do
     get '/users/sign_out' => 'users/sessions#destroy'
