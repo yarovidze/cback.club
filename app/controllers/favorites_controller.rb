d29082adda156938860b6c8b47280f0320b521e1
Serhii Yarovyi <cant1987m@gmail.com>
2021-03-16 16:52:54 +0200

    favorite added

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -1,25 +1,21 @@
 class FavoritesController < ApplicationController
 
   def index
-    @favorite = current_user.favorite
+    @favorite = Favorite.where(user_id: current_user.id)
   end
 
   def create
-    @favorite = Favorite.new(offer_id: params[:offer_id], user_id: current_user)
+    @favorite = Favorite.new(offer_id: params[:offer_id], user_id: current_user.id)
     if @favorite.save
-      redirect_back fallback_location: root_path, notice: 'Added to favorite'
+      redirect_back fallback_location: root_path, notice: 'Додано в обране'
     else
       redirect_back fallback_location: root_path, notice: 'Error'
-      puts "_-----------------------------------"
-      puts current_user.id
-      puts current_user
-      puts params[:offer_id]
     end
   end
 
   def destroy
     offer = Offer.find(params[:id])
-    @favorite = Favorite.where(user_id: current_user.id, offer_id: offer.id).first
+    @favorite = Favorite.where(user_id: current_user.id, offer_id: offer.id).destroy_all
     redirect_back fallback_location: root_path, notice: 'Deleted from favorite'
   end
 
