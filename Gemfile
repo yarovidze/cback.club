08f05bc0d4adaad4fe3d9746220bd3871ed81843
OleStan <59766354+OleStan@users.noreply.github.com>
2021-03-19 13:52:27 +0200

    add api controller

--- Gemfile
+++ Gemfile
@@ -20,8 +20,12 @@ gem 'jbuilder', '~> 2.7'
 # gem 'redis', '~> 4.0'
 # Use Active Model has_secure_password
 # gem 'bcrypt', '~> 3.1.7'
-# gem 'devise'
-# gem 'activeadmin'
+
+# Admidat api
+
+gem 'admitad'
+gem "http"
+
 
 # Use Active Storage variant
 gem 'image_processing', '~> 1.2'
--- app/controllers/offers_controller.rb
+++ app/controllers/offers_controller.rb
@@ -1,28 +1,36 @@
+# frozen_string_literal: true
 class OffersController < ApplicationController
+  before_action :find_offer, only: %i[show offer_redirect]
   def index
     @offers = Offer.where('name ILIKE ?', "%#{params[:query]}%").paginate(page: params[:page], per_page: 16)
     respond_to do |format|
       format.html
       format.json { render json: @offers.map(&:name) }
     end
+    # test--------------------------------
+    autorisation_admitad
+    get_subid_data
+    rec_user_data if cookies[:subid_data].present?
+  end
 
+  def redirect
+    redirect_to @offer.link_to_offer(@offer.link, current_user.id)
   end
 
   def search
     @offers = Offer.where('name ILIKE ?', "%#{params[:query]}%").paginate(page: params[:page], per_page: 16)
-    render "offers/index"
+    render 'offers/index'
   end
 
   def autocomplete
     render json: Offer.all.map(&:name)
- end
- 
-  def show
+  end
+
+  def show; end
+
+  private
+
+  def find_offer
     @offer = Offer.find(params[:id])
   end
-
-  def search
-    render :index
-  end
-
 end
