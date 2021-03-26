9082c0ab173949e87ed65a9c246393f2858ba0b0
Yevhenii Kononenko <evgenijkononenko0@gmail.com>
2021-03-26 18:38:03 +0200

    Active Admin validations && Session time setup

--- app/models/offer.rb
+++ app/models/offer.rb
@@ -3,6 +3,12 @@ class Offer < ApplicationRecord
   has_many :favorites, dependent: :destroy
   belongs_to :category
   has_one_attached :image
+  validates :name, presence: true
+  validates :alt_name, presence: true
+  validates_numericality_of :cashback, greater_than_or_equal_to: 0
+  validates_numericality_of :cashback_percent, greater_than_or_equal_to: 0
+  validates_numericality_of :confirmation, greater_than_or_equal_to: 0
+  
 
   #extend FriendlyId
   #friendly_id :name, use: :slugged
