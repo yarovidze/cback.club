3b27605d7e0754bfad7c214547ed7a562ab7b84f
Serhii Yarovyi <cant1987m@gmail.com>
2021-04-02 14:12:39 +0300

    fixed translate ru, seted timezone

--- config/application.rb
+++ config/application.rb
@@ -9,7 +9,7 @@ I18n.default_locale = :ru
 I18n.locale = :ru
 
 
-module Lesson12
+module Cashback
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.1
@@ -19,7 +19,7 @@ module Lesson12
     # These settings can be overridden in specific environments using the files
     # in config/environments, which are processed later.
     #
-    # config.time_zone = "Central Time (US & Canada)"
+    config.time_zone = "Kyiv"
     # config.eager_load_paths << Rails.root.join("extras")
   end
 end
