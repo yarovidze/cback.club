3a49909138d36bf372b097bd4a7e315499a68984
Yevhenii Kononenko <evgenijkononenko0@gmail.com>
2021-04-16 13:29:39 +0300

    added search to categories

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,6 +1,6 @@
 class CategoriesController < ApplicationController
   def show
     @category = Category.find(params[:id])
-    @offers = Offer.where(category_id: @category).paginate(page: params[:page], per_page: 16)
+    @offers = Offer.where(category_id: @category.id).where('name ILIKE ?  OR alt_name ILIKE ?', "%#{params[:query]}%", "%#{params[:query]}%").paginate(page: params[:page], per_page: 16)
   end
 end
