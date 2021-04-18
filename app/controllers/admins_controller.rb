36826e2dd0c18693ced6545a0e0864a422242f69
OleStan <59766354+OleStan@users.noreply.github.com>
2021-04-18 23:02:52 +0300

    add require

--- app/controllers/admins_controller.rb
+++ app/controllers/admins_controller.rb
@@ -1,5 +1,6 @@
 class AdminsController < ApplicationController
   skip_before_action :verify_authenticity_token
+  require 'liqpay'
   def create_json(request)
     raw_json = JSON.generate(request)
     raw_json = JSON.parse raw_json.gsub('=>', ':')
@@ -11,12 +12,11 @@ class AdminsController < ApplicationController
     signature = params[:signature]
     #signature_my = base64_encode( sha1( 'sandbox_Y9YPYGvIQrweilaQTrUKc80c86a3zvNpkNhyJMH9' + data_req + 'sandbox_Y9YPYGvIQrweilaQTrUKc80c86a3zvNpkNhyJMH9') )
     Trial.create(name: 'liqpay_sing',
-                 test_field1: signature.to_s,
-                 test_field2: liqpay.match?(data, signature)).save
+                 test_field1: signature.to_s).save
 
-    data_raw = liqpay.decode_data(data)
-    data_j = create_json(data_raw.force_encoding('utf-8'))
-    rec_withdrawal(data_j)
+    #data_raw = liqpay.decode_data(data)
+    #data_j = create_json(data_raw.force_encoding('utf-8'))
+    #rec_withdrawal(data_j)
   end
 
   def rec_withdrawal(data)
