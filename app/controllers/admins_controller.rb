504e81852e05cf0ec4f942b6242658cfa91f23bb
OleStan <59766354+OleStan@users.noreply.github.com>
2021-04-23 00:29:01 +0300

    test rec data

--- app/controllers/admins_controller.rb
+++ app/controllers/admins_controller.rb
@@ -8,24 +8,35 @@ class AdminsController < ApplicationController
     signature = params[:signature]
     liqpay = Liqpay.new
     if liqpay.match?(data, signature)
-    Trial.create(name: 'liqpay_sing',
-                 test_field1: signature.to_s,
-                 test_field2: data.to_s).save
     Trial.create(name: 'liqpay_sing_correct?',
                  test_field1: liqpay.match?(data, signature),
                  test_field2: liqpay.decode_data(data)).save
-    rec_withdrawal(data)
+    rec_withdrawal(decode_data(data))
     end
   end
 
   def rec_withdrawal(data)
-    Transaction.create(user_id: 1, action_id: "33333333333333".to_i, offer_id: 1, status: 3, cashback_sum: 2).save
-    @transaction = Transaction.create(user_id: 1, action_id: "2222222222".to_i, offer_id: 1, status: 3, cashback_sum: 2).save
-    @transaction.offer_id = "1"
-    @transaction.status = "0"
-    @transaction.total = data["amount"]
-    @transaction.cashback_sum = data["amount"]
-    @transaction.user_id = "1"
+    interim_transaction = Transaction.where(action_id: data["order_id"])
+    user = User.find(interim_transaction.user_id)
+    Trial.create(name: 'rec_withdrawal test',
+                 test_field1: interim_transaction.action_id.to_s,
+                 test_field2: user.id.to_s).save
+    transactions_open = user.transactions.where(status: 0)
+    if transactions_open.sum(:total) == data["amount"].to_f
+      transactions_open.each do |trans|
+        trans.first.status = 4
+        trans.save
+      end
+    end
+
+
+    #Transaction.create(user_id: 1, action_id: "33333333333333".to_i, offer_id: 1, status: 3, cashback_sum: 2).save
+    #@transaction = Transaction.create(user_id: 1, action_id: "2222222222".to_i, offer_id: 1, status: 3, cashback_sum: 2).save
+    #@transaction.offer_id = "1"
+    #@transaction.status = "0"
+    #@transaction.total = data["amount"]
+    #@transaction.cashback_sum = data["amount"]
+    #@transaction.user_id = "1"
   end
 
 end
--- app/models/transaction.rb
+++ app/models/transaction.rb
@@ -4,7 +4,7 @@ class Transaction < ApplicationRecord
   belongs_to :user
   belongs_to :offer
   #approved_but_stalled ==  pending !!!!!
-  enum status: { pending: 0, approved: 1, declined: 2, approved_but_stalled: 3, paid: 4 }
+  enum status: { pending: 0, approved: 1, declined: 2, approved_but_stalled: 3, paid: 4, interim_proces: 5 }
 
   def user_cashback
     cashback_sum * offer.cashback_percent
