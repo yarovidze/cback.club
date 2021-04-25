38be5c877dda80ba594b74d9f533da3686268f15
Serhii Yarovyi <cant1987m@gmail.com>
2021-04-25 15:27:27 +0300

    added custom error pages

--- config/routes.rb
+++ config/routes.rb
@@ -19,9 +19,9 @@ Rails.application.routes.draw do
                             sessions: 'users/sessions' }
   root 'offers#index'
   get '/agreement', to: 'pages#agreement', as: "agreement"
-  match '/422', to: "pages#unacceptable", via: :all
-  match "/404", to: "pages#not_found", via: :all
-  match "/500", to: "pages#internal_server_error", via: :all
+  match "/404", to: "errors#not_found", via: :all
+  match "/422", to: "errors#unacceptable", via: :all
+  match "/500", to: "errors#internal_server_error", via: :all
   resources :favorites
   resources :users, only: %i[show]
   resources :categories
