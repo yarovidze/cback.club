9dccbc433c343bf7b0cfc09d463c285087dc61d8
Yevhenii Kononenko <evgenijkononenko0@gmail.com>
2021-05-03 16:46:03 +0300

    added validations & active admin logout fix

--- app/models/category.rb
+++ app/models/category.rb
@@ -2,6 +2,7 @@ class Category < ApplicationRecord
   has_many :offers
   has_one_attached :category_icon
   validates :name, presence: true
+  validates :name, length: { in: 5..50 }  
   def to_param
     [id, name.parameterize].join("-")
   end
