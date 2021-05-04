d59a3aaf34955638e183e6bc68b0356663b3c5c0
Yevhenii Kononenko <evgenijkononenko0@gmail.com>
2021-05-04 21:18:20 +0300

    #105, #106 Fixed Credentials Hided

--- config/initializers/recaptcha.rb
+++ config/initializers/recaptcha.rb
@@ -1,6 +1,6 @@
 Recaptcha.configure do |config|
-  config.site_key  = '6LeZ2G8aAAAAAF3G0LLBdxjdqOUJjNwkotgS4L5L'
-  config.secret_key = '6LeZ2G8aAAAAAI6aMnGQxFeXZ4h8uJS42Q7m3GLr'
+  config.site_key = Rails.application.credentials.google[:RECAPTCHA_SITE_KEY]
+  config.secret_key = Rails.application.credentials.google[:RECAPTCHA_SECRET_KEY]
   # Uncomment the following line if you are using a proxy server:
   # config.proxy = 'http://myproxy.com.au:8080'
 end
