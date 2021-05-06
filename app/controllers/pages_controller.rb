43461cc5e98fef328ea877f1dc51afc890cb90b6
OleStan <59766354+OleStan@users.noreply.github.com>
2021-05-06 13:36:27 +0300

    fix card exetption when numbers count < 16, add "all" button

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,10 +1,9 @@
+# frozen_string_literal: true
+
 class PagesController < ApplicationController
+  def index; end
 
-  def index
-  end
-
-  def agreement
-  end
+  def agreement; end
 
   def not_found
     render status: 404
